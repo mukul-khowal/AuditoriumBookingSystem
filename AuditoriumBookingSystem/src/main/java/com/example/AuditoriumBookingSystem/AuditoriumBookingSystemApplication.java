0c24f83b93e439a1a8c884b06722a4908b31434e
mukul <mukulkm9891@gmail.com>
2026-01-21 13:12:08 +0530

    Project Startup

---
+++ AuditoriumBookingSystem/src/main/java/com/example/AuditoriumBookingSystem/AuditoriumBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.AuditoriumBookingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditoriumBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditoriumBookingSystemApplication.class, args);
+	}
+
+}
---
+++ AuditoriumBookingSystem/src/test/java/com/example/AuditoriumBookingSystem/AuditoriumBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AuditoriumBookingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditoriumBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
